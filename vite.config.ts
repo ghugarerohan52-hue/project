be17653be79d387519d2b02fabf12ec731cc411c
chandramani ghugare <Chandramanighuare@gmail.com>
2026-07-19 17:43:28 +0530

    fix: 404 deployment asset path issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
+  base: "/project/",
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
     port: 3000,
     proxy: {
-      '/api': 'http://localhost:3001',
+      "/api": "http://localhost:3001",
     },
   },
-})
+});
